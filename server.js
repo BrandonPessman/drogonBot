b031db828dfc182e9ebad8ca48a4f6e77910a8f5
BrandonPessman <pessman.brandon@gmail.com>
2019-05-19 01:39:51 -0500

    Adds help command

--- server.js
+++ server.js
@@ -9,9 +9,34 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+  if (msg.author == client.user) {
+    // Prevent bot from responding to its own messages
+    return
+  }
+
+  if (msg.content.startsWith('!')) {
+    processCommand(msg)
   }
 })
+
+function processCommand(msg) {
+  let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+  let splitCommand = fullCommand.split(' ') // Split the message up in to pieces for each space
+  let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+  let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+  console.log('Command Recieved: ' + primaryCommand)
+  console.log('Arguments: ' + arguments)
+
+  if (primaryCommand == 'help') {
+    helpCommand(arguments, msg)
+  }
+}
+
+function helpCommand(arguments, msg) {
+  msg.channel.send(
+    "Looks like your are looking for some help. I don't have anything for you yet!"
+  )
+}
 
 client.login(process.env.BOT_TOKEN)
